c25b63ef22c6941ec0daef7357fadae5facb8e95
Karen <95060071+wast3rr@users.noreply.github.com>
2025-07-21 20:14:32 -0400

    Started implementing git objects

--- libwyag.py
+++ libwyag.py
@@ -151,3 +151,58 @@ def repo_find(path=".", required=True):
             return None
 
     return repo_find(parent, required)
+
+class GitObject (object):
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+
+    def serialize(self, repo):
+        """ must read the object's contents from self.data,
+        a byte string, and do whatever it takes to convert i into a 
+        meaningful representation.
+        """
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+    def init(self):
+        pass # reasonable default
+
+def object_read(repo, sha):
+    # read object sha from git repository repo
+    # return GitObject whose exact type depends on the object
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+    
+    if not os.path.isfile(path):
+        return None
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # read and validate object size
+        y = raw.find(b'\x00', x)
+
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception(f"Malforbed object {sha}: bad length")
+
+        #Pick constructor 
+        match fmt:
+            case b'commit'  : c=GitCommit
+            case b'tree'    : c=GitTree
+            case b'tag'     : c=GitTag
+            case b'blob'    : c=GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
+
+        # call constructor and return object
+        return c(raw[y+1:])
